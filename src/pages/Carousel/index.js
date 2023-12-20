0ec99b7a7abc310f4ef31f18d926f3e31f64450d
denilsoncoutinho <denilsoncoutinho9@gmail.com>
2023-12-20 17:24:52 -0300

    ajustando

--- src/pages/Carousel/index.js
+++ src/pages/Carousel/index.js
@@ -1,11 +1,10 @@
 //images
 import Image from "next/image";
 import img1 from '../../assets/whatIsLp 2/image 19.svg'
-import img2 from '../../assets/whatIsLp 2/image 10.png'
+import img2 from '../../assets/whatIsLp 2/image 10.svg'
 import img3 from '../../assets/whatIsLp 2/image 18.svg'
-import img4 from '../../assets/whatIsLp 2/image 7.png'
+import img4 from '../../assets/whatIsLp 2/image 7.svg'
 import img5 from '../../assets/whatIsLp 2/image 8.svg'
-import img6 from '../../assets/whatIsLp 2/image 12.png'
 
 //style
 import style from "../../styles/TechnologiesUsed.module.css";
@@ -22,9 +21,9 @@ export default function Carousel() {
               <Image src={img3} width={400} className="rounded-xl"/>
               <Image src={img4} width={400} />
 
+              <Image src={img2} width={400} className="rounded-xl"  />
               <Image src={img1} width={400} />
               <Image src={img3} width={400} />
-              <Image src={img2} width={400} className="rounded-xl"  />
               <Image src={img5} width={400} className="rounded-xl" />
               <Image src={img4} width={400} />
               
@@ -37,15 +36,6 @@ export default function Carousel() {
           </div>
         </div>
       </div>
-      {/* <div className='px-4 flex lg:flex-row gap-6 flex-col justify-center items-center lg:justify-between max-w-[1000px] m-auto mt-44'>
-        <Image src={PcImageLpResponsive} />
-        <div className="flex flex-col max-w-[29.6rem] ">
-          <h1 className="linearText lg:text-4xl text-2xl  font-semibold max-w-[29.6rem] interactiveScroll_title">O que é uma landing page?</h1>
-          <p className="text-white mt-4 text-left lg:text-base text-sm interactiveScroll_title ">Uma landing page é uma ferramenta essencial para qualquer empresa que queira aumentar suas conversões e gerar mais leads.</p>
-          <p className="text-white mt-4 text-left lg:text-base text-sm interactiveScroll_title">Ao criar uma landing page eficaz e otimizada, você pode melhorar  suas taxas de conversão e aumentar o sucesso de suas vendas.</p>
-          <p className="text-white mt-4 text-left lg:text-base text-sm interactiveScroll_title">Uma landing Page de vendas de alta conversão é a solução para quem quer atrair mais clientes e vender mais através da internet.</p>
-        </div>
-      </div> */}
     </>
   );
 }
--- src/pages/seeAnyExamples/index.js
+++ src/pages/seeAnyExamples/index.js
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import img1 from '../../assets/whatIsLp 2/image 19.svg'
-import img2 from '../../assets/whatIsLp 2/image 10.png'
+import img2 from '../../assets/whatIsLp 2/image 10.svg'
 import img3 from '../../assets/whatIsLp 2/image 18.svg'
-import img4 from '../../assets/whatIsLp 2/image 7.png'
+import img4 from '../../assets/whatIsLp 2/image 7.svg'
 import img5 from '../../assets/whatIsLp 2/image 8.svg'
 import img6 from '../../assets/whatIsLp 2/image 12.svg'
 
