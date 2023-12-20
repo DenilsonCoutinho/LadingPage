41a6815ef7cf26ef8d05856951deb504d297baec
denilsoncoutinho <denilsoncoutinho9@gmail.com>
2023-12-20 00:22:47 -0300

    ajustando

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,7 @@ import { Analytics } from '@vercel/analytics/react';
 export default function App({ Component, pageProps }) {
   return (
     <ScreenSizeProvider>
+      <InteractiveScroll/>
       <Analytics />
       <Component {...pageProps} />
     </ScreenSizeProvider>
