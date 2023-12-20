0cfa54e8a7aab88b284a415513a4f681ef421545
denilsoncoutinho <denilsoncoutinho9@gmail.com>
2023-12-20 14:49:33 -0300

    ajustando

--- src/pages/Carousel/index.js
+++ src/pages/Carousel/index.js
@@ -1,10 +1,10 @@
 //images
 import Image from "next/image";
-import img1 from '../../assets/whatIsLp 2/image 9.png'
+import img1 from '../../assets/whatIsLp 2/image 19.svg'
 import img2 from '../../assets/whatIsLp 2/image 10.png'
 import img3 from '../../assets/whatIsLp 2/image 18.svg'
 import img4 from '../../assets/whatIsLp 2/image 7.png'
-import img5 from '../../assets/whatIsLp 2/image 3.png'
+import img5 from '../../assets/whatIsLp 2/image 8.svg'
 import img6 from '../../assets/whatIsLp 2/image 12.png'
 
 //style
@@ -17,9 +17,9 @@ export default function Carousel() {
           <div className={`${style.slider}`}>
             <div className={`${style.slideTrack} `}>
               <Image src={img1} width={400} />
-              <Image src={img3} width={400} className="rounded-xl"/>
-              <Image src={img2} width={400} className="rounded-xl"  />
               <Image src={img5} width={400} className="rounded-xl" />
+              <Image src={img2} width={400} className="rounded-xl"  />
+              <Image src={img3} width={400} className="rounded-xl"/>
               <Image src={img4} width={400} />
 
               <Image src={img1} width={400} />
--- src/pages/seeAnyExamples/index.js
+++ src/pages/seeAnyExamples/index.js
@@ -1,10 +1,10 @@
 import Image from "next/image";
-import img1 from '../../assets/whatIsLp 2/image 9.png'
+import img1 from '../../assets/whatIsLp 2/image 19.svg'
 import img2 from '../../assets/whatIsLp 2/image 10.png'
 import img3 from '../../assets/whatIsLp 2/image 18.svg'
 import img4 from '../../assets/whatIsLp 2/image 7.png'
-import img5 from '../../assets/whatIsLp 2/image 8.png'
-import img6 from '../../assets/whatIsLp 2/image 12.png'
+import img5 from '../../assets/whatIsLp 2/image 8.svg'
+import img6 from '../../assets/whatIsLp 2/image 12.svg'
 
 export default function SeeAnyExamples() {
   return (
@@ -13,9 +13,9 @@ export default function SeeAnyExamples() {
         <h1 className="linearText text-center lg:text-5xl text-3xl  font-semibold " id="pages">Designs de alta <br />performance</h1>
         <p className="text-white max-w-[600px] text-center" >oferecemos páginas inteligentes para negócios digitais. Desenvolvemos toda a estrutura web a partir da sua identidade visual.</p>
         <div className="flex flex-wrap justify-center lg:gap-16 gap-6 mt-24" >
-          <Image src={img5} width={400} alt="image-1" />
-          <Image src={img3} width={400} alt="image-5" />
           <Image src={img1} width={400} alt="image-2" />
+          <Image src={img3} width={400} alt="image-5" />
+          <Image src={img5} width={400} alt="image-1" />
           <Image src={img4} width={400} alt="image-3" />
           <Image src={img2} width={400} alt="image-4" />
           <Image src={img6} width={400} alt="image-6" />
