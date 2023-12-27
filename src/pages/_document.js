a49bfa65647781d1f9dbf735da8ed7131d76f7b2
denilsoncoutinho <denilsoncoutinho9@gmail.com>
2023-12-27 01:14:46 -0300

    ajustando

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Adicione o link para o favicon aqui */}
+        <link rel="icon" href="/public/favicon.ico" type="image/x-icon" />
+        <link rel="shortcut icon" href="/public/favicon.ico" type="image/x-icon" />
+        {/* Outros elementos do cabeçalho podem ser adicionados aqui */}
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
